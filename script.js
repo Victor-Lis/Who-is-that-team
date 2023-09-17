494bc65bdf6b8cc261c5be62a901fa10434cc717
Victor-Lis <victorlisbronzo1@gmail.com>
2023-09-17 12:56:39 -0300

    v2
    Agora é possível upar imagens locais

--- script.js
+++ script.js
@@ -1,7 +1,64 @@
 const input = document.querySelector("#url")
+const url = `https://api.imgur.com/3/image`;
+const urlCustomVisionAI = `https://eastus2.api.cognitive.microsoft.com/customvision/v3.0/Prediction/296b68b1-0da2-4455-92f7-3ddcb30ab0f1/classify/iterations/Iteration5/url`
+const apiKey = "2df1400f224783a";
+
+const setPage = (type) => {
+
+    if(type == "URL"){
+
+        document.querySelector("#nav-URL").style.color = "#297ca9"
+        document.querySelector("#nav-Upload").style.color = "#3EBDFF"
+
+        document.querySelector(".content-box").innerHTML = `
+        
+        <h2 style="color: #fff; font-weight: bold;"> <strong style="color: #3EBDFF">Qual</strong> é esse <strong style="color: #3EBDFF">time</strong>? </h2>
+
+        <div class="input-box">
+
+            <h3 class="input-title"> Insira a URL da imagem </h3>
+            <input type="url" class="input-url" id='url'>
+            <img src="" id="img" style="width: 100%; border-radius: 10px; margin: 05px 0;">
+            <button class="input-button" onclick="getPrediction()"> Enviar </button>
+
+        </div>
+
+        `
+
+    }else{
+
+        document.querySelector("#nav-URL").style.color = "#3EBDFF"
+        document.querySelector("#nav-Upload").style.color = "#297ca9"
+
+        document.querySelector(".content-box").innerHTML = `
+        
+        <h2 style="color: #fff; font-weight: bold;"> <strong style="color: #3EBDFF">Qual</strong> é esse <strong style="color: #3EBDFF">time</strong>? </h2>
+
+        <div class="input-box">
+
+            <input class='input-url' style="background-color: #fff; margin: 05px 0" type="file" id="image">
+            <img src="" id="img" style="width: 100%; border-radius: 10px; margin: 05px 0;">
+            <button class="input-button" onclick="uploadImage()">Upload</button>
+
+        </div>
+
+        `
+
+    }
+
+}
+
+
+const setImg = () => {
+
+    let url = document.querySelector("#url").value
+    document.querySelector("#img").src = url
+
+}
+
 
 async function getPrediction() {
-
+    
     if (document.querySelector("#url").value.length > 0) {
 
         let url = document.querySelector("#url").value
@@ -14,6 +71,7 @@ async function getPrediction() {
 
             <h3 class="input-title"> Insira a URL da imagem </h3>
             <input type="url" class="input-url" id='url'>
+            <img src="${url}" id="img" style="width: 100%; border-radius: 10px; margin: 05px 0;">
             <button class="input-button" onclick="getPrediction()"> Enviar </button>
 
         </div>
@@ -27,7 +85,7 @@ async function getPrediction() {
         `
 
         let data = await fetch(
-            "https://eastus2.api.cognitive.microsoft.com/customvision/v3.0/Prediction/296b68b1-0da2-4455-92f7-3ddcb30ab0f1/classify/iterations/Iteration5/url",
+            urlCustomVisionAI,
             {
 
                 method: "POST",
@@ -56,6 +114,7 @@ async function getPrediction() {
     
                 <h3 class="input-title"> Insira a URL da imagem </h3>
                 <input type="url" class="input-url" id='url'>
+                <img src="" id="img" style="width: 100%; border-radius: 10px; margin: 05px 0;">
                 <button class="input-button" onclick="getPrediction()"> Enviar </button>
     
             </div>
@@ -80,6 +139,7 @@ async function getPrediction() {
     
                 <h3 class="input-title"> Insira a URL da imagem </h3>
                 <input type="url" class="input-url" id='url'>
+                <img src="" id="img" style="width: 100%; border-radius: 10px; margin: 05px 0;">
                 <button class="input-button" onclick="getPrediction()"> Enviar </button>
     
             </div>
@@ -104,6 +164,7 @@ async function getPrediction() {
     
                 <h3 class="input-title"> Insira a URL da imagem </h3>
                 <input type="url" class="input-url" id='url'>
+                <img src="${url}" id="img" style="width: 100%; border-radius: 10px; margin: 05px 0;">
                 <button class="input-button" onclick="getPrediction()"> Enviar </button>
     
             </div>
@@ -131,5 +192,155 @@ async function getPrediction() {
 
         document.querySelector("#url").value = ''
     }
+
+}
+
+
+const uploadImage = async () => {
+    const formData = new FormData();
+    formData.append("image", document.querySelector("#image").files[0]);
+    
+    // Faça a solicitação HTTP
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Authorization", `Client-ID ${apiKey}`);
+    xhr.send(formData);
+
+    // Processe a resposta
+    xhr.onload = async function () {
+
+        document.querySelector(".content-box").innerHTML = `
+            
+        <h2 style="color: #fff; font-weight: bold;"> <strong style="color: #3EBDFF">Qual</strong> é esse <strong style="color: #3EBDFF">time</strong>? </h2>
+
+        <div class="input-box">
+
+            <input class='input-url' style="background-color: #fff; margin: 05px 0" type="file" id="image">
+            <img src="" id="img" style="width: 100%; border-radius: 10px; margin: 5px 0;">
+            <button class="input-button" onclick="uploadImage()">Upload</button>
+
+        </div>
+        
+        <div class='card'>  
+        
+            <h4> Carregando... </h4>
+
+        </div>
+
+        `
+        
+        if (xhr.status === 200) {
+            // Obtenha o link para a imagem
+            const link = JSON.parse(xhr.responseText).data.link;
+            const img = link
+            let data = await fetch(
+                urlCustomVisionAI,
+                {
+    
+                    method: "POST",
+                    headers: {
+                        "Prediction-Key": "28dba1f169e444de817ef3a80b136773",
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+    
+                        url: link,
+    
+                    }),
+    
+                }
+            ).then(res =>
+    
+                res.json()
+    
+            ).catch(err => {
+
+                document.querySelector(".content-box").innerHTML = `
+            
+                <h2 style="color: #fff; font-weight: bold;"> <strong style="color: #3EBDFF">Qual</strong> é esse <strong style="color: #3EBDFF">time</strong>? </h2>
+    
+                <div class="input-box">
+
+                    <input class='input-url' style="background-color: #fff; margin: 05px 0" type="file" id="image">
+                    <img src="" id="img" style="width: 100%; border-radius: 10px; margin: 5px 0;">
+                    <button class="input-button" onclick="uploadImage()">Upload</button>
+    
+                </div>
+                
+                <div class='card'>  
+                
+                    <h4 style='color: #fff'> <strong style='color: red'> Error</strong>.<strong style='color: red'>.</strong>. Formato Inválido </h4>
+        
+                </div>
+        
+                `
+    
+            })
+
+            if(data.code == "BadRequestImageUrl"){
+                
+                document.querySelector(".content-box").innerHTML = `
+                
+                <h2 style="color: #fff; font-weight: bold;"> <strong style="color: #3EBDFF">Qual</strong> é esse <strong style="color: #3EBDFF">time</strong>? </h2>
+    
+                <div class="input-box">
+
+                    <input class='input-url' style="background-color: #fff; margin: 05px 0" type="file" id="image">
+                    <img src="" id="img" style="width: 100%; border-radius: 10px; margin: 5px 0;">
+                    <button class="input-button" onclick="uploadImage()">Upload</button>
+    
+                </div>
+                
+                <div class='card'>  
+                
+                    <h4 style='color: #fff'> <strong style='color: red'> Error</strong>.<strong style='color: red'>.</strong>. Formato Inválido </h4>
+        
+                </div>
+    
+                `
+    
+            }
+
+            if (data.predictions) {
+
+                console.log(img)
+                document.querySelector(".content-box").innerHTML = `
+                
+                <h2 style="color: #fff; font-weight: bold;"> <strong style="color: #3EBDFF">Qual</strong> é esse <strong style="color: #3EBDFF">time</strong>? </h2>
+    
+                <div class="input-box">
+
+                    <input class='input-url' style="background-color: #fff; margin: 05px 0" type="file" id="image">
+                    <img src="" id="img" style="width: 100%; border-radius: 10px; margin: 5px 0;">
+                    <button class="input-button" onclick="uploadImage()">Upload</button>
+    
+                </div>
+    
+                `
+    
+                // console.log(data.predictions)
+                data.predictions.map(predict => {
+    
+                    let div = document.createElement("div")
+                    div.classList.add("card")
+    
+                    let tag = document.createElement('h4')
+                    tag.innerText = predict.tagName
+    
+                    let percent = document.createElement('h4')
+                    percent.innerText = Math.floor(parseFloat(`${predict.probability}`.slice(0, 4)) * 100) > 100? "0%": Math.floor(parseFloat(`${predict.probability}`.slice(0, 4)) * 100)+"%"
+    
+                    div.appendChild(tag)
+                    div.appendChild(percent)
+                    document.querySelector(".content-box").appendChild(div)
+    
+                })
+            }
+
+        } else {
+            // Erro ao fazer upload da imagem
+            console.log("Erro ao fazer upload da imagem: " + xhr.status);
+        }
+    };
 
 }
